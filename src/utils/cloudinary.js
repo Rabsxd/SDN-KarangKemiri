343725f98f5fa7e0c1220587d20d4f4e66c87eeb
Rabsxd <reyanbz.rb@gmail.com>
2025-08-07 23:27:30 +0700

    update cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,18 @@
+// src/utils/cloudinary.js
+
+// Ambil konfigurasi dari Environment Variables
+const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
+
 export const uploadToCloudinary = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("upload_preset", "profil-sekolah-unsigned"); // GANTI DENGAN NAMA PRESET-MU
-  formData.append("cloud_name", "dqxub9upe"); // GANTI DENGAN CLOUD NAME-MU
+  formData.append("upload_preset", UPLOAD_PRESET);
+  formData.append("cloud_name", CLOUD_NAME);
 
   try {
     const response = await fetch(
-      `https://api.cloudinary.com/v1_1/${"dqxub9upe"}/image/upload`, // GANTI DENGAN CLOUD NAME-MU
+      `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
       {
         method: "POST",
         body: formData,
